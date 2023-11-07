5b213ce9b1afb8aff635b3bf6a8ee29f26d853ff
Timok951 <timofey.alekseev@list.ru>
2023-11-07 23:28:03 +0300

    push some bad code

---
+++ File conventer/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_conventer
+{
+
+    internal class Human
+    {
+        public string Name;
+        public int Age;
+        public Human(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+        }
+       
+      
+
+
+    }
+}
---
+++ File conventer/Program.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+
+namespace File_conventer
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Human> hum = new List<Human>(); ///конструктор
+            Human Timof = new Human("Тимофей", 19);
+            hum.Add(Timof);
+
+            Console.WriteLine("Имя файла и путь");
+            string path = Console.ReadLine();
+            ///тут должна быть функция
+            if (path.EndsWith(".txt"))
+            {
+
+                if (File.Exists(path)) {
+                    List <Human> hum = File.ReadAllText(path);
+
+
+                }
+                else
+                {
+                    File.Create(path);
+                }
+               
+
+            }
+
+            else if (path.EndsWith(".json")) {
+                string text = File.ReadAllText(path);
+                List<Human> hum = JsonConvert.DeserializeObject<List<Human>>(text);
+
+            }
+
+            else if (path.EndsWith(".xml")) {
+
+                XmlSerializer xml = new XmlSerializer(typeof(List<Human>));
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+
+                    hum = (List<Human>)xml.Deserialize(fs);
+
+                }
+            }
+        foreach (var item in hum)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Age);
+            }
+            Console.WriteLine("куда и как сохраняем?");
+            path = Console.ReadLine();
+
+            if (path.EndsWith(".txt")) {
+
+               File.WriteAllText(path, hum);
+            
+            }
+
+            if (path.EndsWith(".json"))
+            {
+
+                string json = JsonConvert.SerializeObject(hum);
+                File.WriteAllText(path, json);
+                
+            }
+
+
+            if (path.EndsWith(".xml"))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Human>));
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+
+                    xml.Serialize(fs, hum);
+
+                }
+                
+
+            }
+
+
+
+        }
+    }
+}
